8b671ea0ecd387065017677900f24055a3341c7d
Emmanuel Appiagyei <emmanuelappiagyei@cmail.carleton.ca>
2026-07-22 17:12:23 -0400

    dynamically rendered colors from the API, fixed bugs with id name duplicates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,25 @@
 const seedColorInput = document.getElementById('seed-color');
 const btnEl = document.getElementById('get-color-btn');
+const colorSchemesEl = document.getElementById('color-schemes-container');
 
-const getSeedColor = () => {
-    return seedColorInput.value;
-}
+const getSeedColor = () => seedColorInput.value.slice(1);
 
 seedColorInput.addEventListener('change', getSeedColor);
 
-
-
 const getColorScheme = () => {
-    //complete API Call
+    const schemeMode = document.getElementById('color-schemes').value;
+    colorSchemesEl.innerHTML = '';
+
+    //API Call
+    fetch(`https://www.thecolorapi.com/scheme?hex=${getSeedColor()}&format=json&mode=${schemeMode.toLowerCase()}&count=5`)
+        .then(res => res.json())
+        .then (data => {
+            data.colors.forEach(color => {
+            const colorCols = document.createElement('div');
+            colorCols.style.backgroundColor = color.hex.value;
+            colorSchemesEl.append(colorCols)
+          });
+        })
 }
 
 
