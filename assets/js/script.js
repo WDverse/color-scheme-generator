3234cd8822b68c72608fe7b3acb0767f5982cbb3
Emmanuel Appiagyei <emmanuelappiagyei@cmail.carleton.ca>
2026-07-23 16:12:18 -0400

    updated styling and rendered colors on page load

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,21 @@ const btnEl = document.getElementById('get-color-btn');
 const hexValuesEl = document.getElementById('hex-values');
 const colorSchemesEl = document.getElementById('color-schemes-container');
 
+
+ fetch(`https://www.thecolorapi.com/scheme?hex=1a1919&format=json&mode=monochrome&count=5`)
+        .then(res => res.json())
+        .then (data => {
+          console.log(data.colors)
+            data.colors.forEach(color => {
+            const colorCols = document.createElement('div');
+            const hexValues = document.createElement('span');
+            hexValues.textContent = color.hex.value;
+            colorCols.style.backgroundColor = color.hex.value;
+            hexValuesEl.append(hexValues);
+            colorSchemesEl.append(colorCols);
+          });
+        })
+
 const getSeedColor = () => seedColorInput.value.slice(1);
 
 seedColorInput.addEventListener('change', getSeedColor);
