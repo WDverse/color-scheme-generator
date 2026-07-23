a9d7fb98a5974ffd15ef2dba9e2a343edb09b436
Emmanuel Appiagyei <emmanuelappiagyei@cmail.carleton.ca>
2026-07-23 15:23:39 -0400

    rendered hex values for each color and updated styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 const seedColorInput = document.getElementById('seed-color');
 const btnEl = document.getElementById('get-color-btn');
+const hexValuesEl = document.getElementById('hex-values');
 const colorSchemesEl = document.getElementById('color-schemes-container');
 
 const getSeedColor = () => seedColorInput.value.slice(1);
@@ -9,15 +10,20 @@ seedColorInput.addEventListener('change', getSeedColor);
 const getColorScheme = () => {
     const schemeMode = document.getElementById('color-schemes').value;
     colorSchemesEl.innerHTML = '';
+    hexValuesEl.innerHTML = '';
 
     //API Call
     fetch(`https://www.thecolorapi.com/scheme?hex=${getSeedColor()}&format=json&mode=${schemeMode.toLowerCase()}&count=5`)
         .then(res => res.json())
         .then (data => {
+          console.log(data.colors)
             data.colors.forEach(color => {
             const colorCols = document.createElement('div');
+            const hexValues = document.createElement('span');
+            hexValues.textContent = color.hex.value;
             colorCols.style.backgroundColor = color.hex.value;
-            colorSchemesEl.append(colorCols)
+            hexValuesEl.append(hexValues);
+            colorSchemesEl.append(colorCols);
           });
         })
 }
